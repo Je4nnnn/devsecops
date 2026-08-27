5364febcfceee16b4a8002ce1ba3a474318c068b
ArielPinochetVTYG <ariel.pinochet@vtyg.cl>
2026-08-27 12:42:09 -0400

    Update Dashboard.vue

--- frontend/src/presentation/views/Dashboard.vue
+++ frontend/src/presentation/views/Dashboard.vue
@@ -12,11 +12,11 @@
           <span>Último sync: <strong>{{ timeAgo(evolutionSummary.last_sync_at) }}</strong></span>
           <span class="sync-date-full">{{ formatDate(evolutionSummary.last_sync_at) }}</span>
         </div>
-        <button class="btn btn-primary" @click="onSyncClick" :disabled="isSyncing">
+        <!--<button class="btn btn-primary" @click="onSyncClick" :disabled="isSyncing">
           <svg v-if="isSyncing" class="spin" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="2" x2="12" y2="6"></line><line x1="12" y1="18" x2="12" y2="22"></line><line x1="4.93" y1="4.93" x2="7.76" y2="7.76"></line><line x1="16.24" y1="16.24" x2="19.07" y2="19.07"></line><line x1="2" y1="12" x2="6" y2="12"></line><line x1="18" y1="12" x2="22" y2="12"></line><line x1="4.93" y1="19.07" x2="7.76" y2="16.24"></line><line x1="16.24" y1="7.76" x2="19.07" y2="4.93"></line></svg>
           <svg v-else xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21.5 2v6h-6M21.34 15.57a10 10 0 1 1-.59-9.5l1.75 1.93"></path></svg>
           {{ isSyncing ? 'Sincronizando en segundo plano...' : 'Forzar Sincronización' }}
-        </button>
+        </button>-->
       </div>
     </div>
 
